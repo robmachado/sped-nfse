564db0780d5e0611d7582e85c41724f8da5c5fa0
Roberto L. Machado <linux.rlm@gmail.com>
2016-09-05 18:31:15 -0300

    [UPD] Campinas

---
+++ src/Counties/M3509502/Tools.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M2111300;
+
+/**
+ * Classe para a comunicação com os webservices da
+ * Cidade de São Luis MA
+ * conforme o modelo DSFNET
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M2111300\Tools
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Roberto L. Machado <linux.rlm at gmail dot com>
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Dsfnet\Tools as ToolsDsfnet;
+
+class Tools extends ToolsDsfnet
+{
+    /**
+     * Webservices URL
+     * @var array
+     */
+    protected $url = [
+        1 => 'http://issdigital.campinas.sp.gov.br/WsNFe2/LoteRps.jws',
+        2 => ''
+    ];
+    /**
+     * County Namespace
+     * @var string
+     */
+    protected $xmlns = 'http://proces.wsnfe2.dsfnet.com.br';
+    /**
+     * Soap Version
+     * @var int
+     */
+    protected $soapversion = 1;
+    /**
+     * SIAFI County Cod
+     * @var int
+     */
+    protected $codcidade = 921;
+    
+    protected $withcdata = false;
+    
+    protected $signaturealgo = 'SHA1';
+}
