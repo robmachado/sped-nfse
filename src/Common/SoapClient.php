18d8c749da8b7cfd4861a46a77b180cf33ba2e76
Roberto L. Machado <linux.rlm@gmail.com>
2016-09-17 17:31:29 -0300

    [UPD] Ajustes em soap

--- src/Common/SoapClient.php
+++ src/Common/SoapClient.php
@@ -2,16 +2,32 @@
 
 namespace NFePHP\NFSe\Common;
 
+use NFePHP\Common\Certificate;
+use Psr\Log\LoggerInterface;
+
 class SoapClient
 {
+    const SSL_DEFAULT = 0; //default
+    const SSL_TLSV1 = 1; //TLSv1
+    const SSL_SSLV2 = 2; //SSLv2
+    const SSL_SSLV3 = 3; //SSLv3
+    const SSL_TLSV1_0 = 4; //TLSv1.0
+    const SSL_TLSV1_1 = 5; //TLSv1.1
+    const SSL_TLSV1_2 = 6; //TLSv1.2
+    
+    protected $soapprotocol = self::SSL_DEFAULT;
+
     protected $certificate;
     protected $soaptimeout = 20;
-    
+    protected $proxyIP = '';
+    protected $proxyPort = '';
+    protected $proxyUser = '';
+    protected $proxyPass = '';
+
     protected $tempdir = '';
     protected $prifile = '';
     protected $pubfile = '';
     protected $certfile = '';
-    protected $connection;
 
     public function __construct(Certificate $certificate, LoggerInterface $logger = null)
     {
@@ -28,5 +44,44 @@ class SoapClient
     public function soapTimeout($timesecs)
     {
         $this->soaptimeout = $timesecs;
+    }
+    
+    public function soapSecurityProtocol($protocol = self::SSL_DEFAULT)
+    {
+        $this->soapprotocol = $protocol;
+    }
+    
+    public function soapProxy($ip, $port, $user, $password)
+    {
+        $this->proxyIP = $ip;
+        $this->proxyPort = $port;
+        $this->proxyUser = $user;
+        $this->proxyPass = $password;
+    }
+    
+    protected function saveTemporarilyKeyFiles()
+    {
+        if (is_object($this->certificate)) {
+            $this->tempdir = sys_get_temp_dir().DIRECTORY_SEPARATOR.'certs'.DIRECTORY_SEPARATOR;
+            if (! is_dir($this->tempdir)) {
+                mkdir($this->tempdir);
+            }
+            $this->prifile = tempnam($this->tempdir, 'Pri').'.pem';
+            $this->pubfile = tempnam($this->tempdir, 'Pub').'.pem';
+            $this->certfile = tempnam($this->tempdir, 'Cert').'.pem';
+            file_put_contents($this->prifile, $this->certificate->privateKey);
+            file_put_contents($this->pubfile, $this->certificate->publicKey);
+            file_put_contents($this->certfile, $this->certificate->privateKey.$this->certificate->publicKey);
+        }
+    }
+
+    protected function removeTemporarilyKeyFiles()
+    {
+        unlink($this->prifile);
+        unlink($this->pubfile);
+        unlink($this->certfile);
+        unlink(substr($this->prifile, 0, strlen($this->prifile)-4));
+        unlink(substr($this->pubfile, 0, strlen($this->pubfile)-4));
+        unlink(substr($this->certfile, 0, strlen($this->certfile)-4));
     }
 }
--- src/Common/SoapCurl.php
+++ src/Common/SoapCurl.php
@@ -2,104 +2,56 @@
 
 namespace NFePHP\NFSe\Common;
 
+use NFePHP\NFSe\Common\SoapClient as LocalClient;
 use NFePHP\Common\Certificate;
-use NFePHP\Common\Dom\Dom;
 use Psr\Log\LoggerInterface;
 use RuntimeException;
+use SimpleXMLElement;
 
-class SoapCurl
+class SoapCurl extends LocalClient
 {
-    const SSL_DEFAULT = 0; //default
-    const SSL_TLSV1 = 1; //TLSv1
-    const SSL_SSLV2 = 2; //SSLv2
-    const SSL_SSLV3 = 3; //SSLv3
-    const SSL_TLSV1_0 = 4; //TLSv1.0
-    const SSL_TLSV1_1 = 5; //TLSv1.1
-    const SSL_TLSV1_2 = 6; //TLSv1.2
-    
-    protected $soaptimeout = 20;
-    protected $soapport = 443;
-    protected $soapprotocol = self::SSL_DEFAULT;
-    protected $logger;
-    
+  
     protected $responseHead = '';
     protected $responseBody = '';
     
-    protected $proxyIP = '';
-    protected $proxyPort = '';
-    protected $proxyUser = '';
-    protected $proxyPass = '';
-    
-    protected $certificate;
-    protected $tempdir = '';
-    protected $prifile = '';
-    protected $pubfile = '';
-    protected $certfile = '';
-
-    public function __construct(Certificate $certificate, LoggerInterface $logger = null)
-    {
-        $this->logger = $logger;
-        $this->certificate = $certificate;
-        $this->saveTemporarilyKeyFiles();
-    }
-    
-    public function __destruct()
-    {
-        $this->removeTemporarilyKeyFiles();
-    }
-    
-    public function soapTimeout($timesecs)
-    {
-        $this->soaptimeout = $timesecs;
-    }
-    
-    public function soapPort($port)
-    {
-        $this->soapport = $port;
-    }
-    
-    public function soapProxy($ip, $port, $user, $password)
-    {
-        $this->proxyIP = $ip;
-        $this->proxyPort = $port;
-        $this->proxyUser = $user;
-        $this->proxyPass = $password;
-    }
-    
-    public function soapSecurityProtocol($protocol = self::SSL_DEFAULT)
-    {
-        $this->soapprotocol = $protocol;
-    }
-    
     /**
      * sendSoap
-     * @param type $url
-     * @param type $port
-     * @param type $envelope
-     * @param type $params
+     * @param string $url
+     * @param string $operation
+     * @param string $action
+     * @param int $soapver
+     * @param array $parameters
      * @return string
-     * @throws \RuntimeException
      */
-    public function soapSend($url, $operation, $soapver, $parameters)
+    public function soapSend($url, $operation = '', $action = '', $soapver = SOAP_1_2, $parameters = [])
     {
         $soapinfo = array();
         $soaperror = '';
         $response = '';
         
-        $port = 443;
+        $envelope = $this->mkEnvSoap1($operation, $parameters);
+        if ($soapver == SOAP_1_2) {
+            $envelope = $this->mkEnvSoap2($operation, $parameters);
+        }
         
-        $envelope = $this->mkEnvSoap2($operation, $parameters);
         $msgSize = strlen($envelope);
+        /* TIRAR ISSO DEPOIS => EXISTE APENAS PARA TESTES */
+        file_put_contents('/tmp/envelope.xml', $envelope);
         
         $curlparams = [
             "Content-Type: application/soap+xml;"
-                . "charset=utf-8;"
-                . "action=\"http://www.prefeitura.sp.gov.br/nfe/ws/". lcfirst($operation) ."\"",
+                . "charset=utf-8;",
             "Content-length: $msgSize"
         ];
         
+        if (!empty($action)) {
+            $curlparams[0] .= ";action=$action";
+        }
+        
         $oCurl = curl_init();
+        
         $this->curlSetProxy($oCurl);
+        
         curl_setopt($oCurl, CURLOPT_URL, $url);
         curl_setopt($oCurl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         curl_setopt($oCurl, CURLOPT_CONNECTTIMEOUT, $this->soaptimeout);
@@ -107,18 +59,14 @@ class SoapCurl
         curl_setopt($oCurl, CURLOPT_HEADER, 1);
         curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($oCurl, CURLOPT_PORT, $port);
-        if ($port == 443) {
-            curl_setopt($oCurl, CURLOPT_SSLVERSION, $this->soapprotocol);
-            curl_setopt($oCurl, CURLOPT_SSLCERT, $this->certfile);
-            curl_setopt($oCurl, CURLOPT_SSLKEY, $this->prifile);
-        }
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, $this->soapprotocol);
+        curl_setopt($oCurl, CURLOPT_SSLCERT, $this->certfile);
+        curl_setopt($oCurl, CURLOPT_SSLKEY, $this->prifile);
         curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
-        if (!empty($envelope)) {
+        //pode ser vazio quando quizer pegar o WSDL apenas
+        if (! empty($envelope)) {
             curl_setopt($oCurl, CURLOPT_POST, 1);
             curl_setopt($oCurl, CURLOPT_POSTFIELDS, $envelope);
-        }
-        if (!empty($curlparams)) {
             curl_setopt($oCurl, CURLOPT_HTTPHEADER, $curlparams);
         }
         
@@ -151,6 +99,9 @@ class SoapCurl
     
     private function mkEnvSoap1($operation, $parameters)
     {
+        if (empty($operation)) {
+            return '';
+        }
         $request = $this->mkRequest($operation, $parameters);
         $envelope = "<soapenv:Envelope "
                 . "xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" "
@@ -166,26 +117,26 @@ class SoapCurl
     
     private function mkEnvSoap2($operation, $parameters)
     {
+        if (empty($operation)) {
+            return '';
+        }
         $request = $this->mkRequest($operation, $parameters);
         $envelope = "<soap:Envelope "
             . "xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" "
-            . "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-            . "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
-            . "<soap:Header/>"
+            . "xmlns=\"http://www.prefeitura.sp.gov.br/nfe\" >"
             . "<soap:Body>$request</soap:Body>"
             . "</soap:Envelope>";
-        echo $envelope;
-        die;
         return $envelope;
     }
     
     private function mkRequest($operation, $parameters)
     {
-        $request = "<$operation>";
+        $tag = $operation . "Request";
+        $request = "<$tag>";
         foreach ($parameters as $key => $value) {
             $request .= "<$key>$value</$key>";
         }
-        $request .= "</$operation>";
+        $request .= "</$tag>";
         return $request;
     }
     
