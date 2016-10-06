fa62045be89b95f9aa0bf10fb35afbdb75069cb5
Roberto L. Machado <linux.rlm@gmail.com>
2016-10-06 10:55:39 -0300

    [UPD] testes

--- tests/Models/Prodam/Factories/ConsultaLoteTest.php
+++ tests/Models/Prodam/Factories/ConsultaLoteTest.php
@@ -1,17 +1,8 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace NFePHP\NFSe\Tests\Models\Prodam\Factories;
 
-/**
- * Description of ConsultaLoteTest
- *
- * @author administrador
- */
 class ConsultaLoteTest
 {
-    //put your code here
+
 }
--- tests/Models/Prodam/Factories/ConsultaNFSePeriodoTest.php
+++ tests/Models/Prodam/Factories/ConsultaNFSePeriodoTest.php
@@ -1,17 +1,8 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace NFePHP\NFSe\Tests\Models\Prodam\Factories;
 
-/**
- * Description of ConsultaNFSePeriodoTest
- *
- * @author administrador
- */
 class ConsultaNFSePeriodoTest
 {
-    //put your code here
+
 }
