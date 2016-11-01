471b7a7498a44fa5d66ab21e2a26d3f3d9b647fe
Roberto L. Machado <linux.rlm@gmail.com>
2016-11-01 16:57:41 -0200

    [INC] Mais classes Issnet

--- src/Models/Issnet/Factories/ConsultarUrlVisualizacaoNfseSerie.php
+++ src/Models/Issnet/Factories/ConsultarUrlVisualizacaoNfseSerie.php
@@ -7,7 +7,25 @@ use NFePHP\NFSe\Models\Issnet\Factories\Factory;
 
 class ConsultarUrlVisualizacaoNfseSerie extends Factory
 {
-    public function render()
-    {
+    public function render(
+        $versao,
+        $remetenteTipoDoc,
+        $remetenteCNPJCPF,
+        $inscricaoMunicipal,
+        $numero,
+        $codigoTributacao,
+        $serie
+    ) {
+        $method = "ConsultarUrlVisualizacaoNfseSerieEnvio";
+        $xsd = 'servico_consultar_url_visualizacao_nfse_serie_envio';
+        $content = $this->requestFirstPart($method, $xsd);
+        $content .= Header::render($remetenteTipoDoc, $remetenteCNPJCPF, $inscricaoMunicipal);
+        $content .= "<Numero>$numero</Numero>";
+        $content .= "<CodigoTributacaoMunicipio>$codigoTributacao</CodigoTributacaoMunicipio>";
+        $content .= "<CodigoSerie>$serie</CodigoSerie>";
+        $content .= "</$method>";
+        $body = $this->clear($content);
+        $this->validar($versao, $body, 'Issnet', $xsd, '');
+        return $body;
     }
 }
