b9c1762bef9ab9f8cf640dea17b295104e40f621
Roberto L. Machado <linux.rlm@gmail.com>
2016-11-02 10:21:14 -0200

    [INC] RPS e RenderRPS para Issnet

--- src/Models/Issnet/RenderRPS.php
+++ src/Models/Issnet/RenderRPS.php
@@ -48,5 +48,367 @@ class RenderRPS
      */
     private static function render(Rps $rps)
     {
+        self::$dom = new Dom('1.0', 'utf-8');
+        $root = self::$dom->createElement('RPS');
+        $infRPS = self::$dom->createElement('infRPS');
+        
+        $identificacaoRps = self::$dom->createElement('IdentificacaoRps');
+        self::$dom->addChild(
+            $identificacaoRps,
+            'Numero',
+            $rps->infNumero,
+            true,
+            "Numero do RPS",
+            true
+        );
+        self::$dom->addChild(
+            $identificacaoRps,
+            'Serie',
+            $rps->infSerie,
+            true,
+            "Serie do RPS",
+            true
+        );
+        self::$dom->addChild(
+            $identificacaoRps,
+            'Tipo',
+            $rps->infTipo,
+            true,
+            "Tipo do RPS",
+            true
+        );
+        self::$dom->appChild($infRPS, $identificacaoRps, 'Adicionando tag IdentificacaoRPS');
+        
+        self::$dom->addChild(
+            $infRPS,
+            'DataEmissao',
+            $rps->infDataEmissao,
+            true,
+            'Data de Emissão do RPS',
+            false
+        );
+        self::$dom->addChild(
+            $infRPS,
+            'NaturezaOperacao',
+            $rps->infNaturezaOperacao,
+            true,
+            'Natureza da operação',
+            false
+        );
+        self::$dom->addChild(
+            $infRPS,
+            'OptanteSimplesNacional',
+            $rps->infOptanteSimplesNacional,
+            true,
+            'OptanteSimplesNacional',
+            false
+        );
+        self::$dom->addChild(
+            $infRPS,
+            'IncentivadorCultural',
+            $rps->infIncentivadorCultural,
+            true,
+            'IncentivadorCultural',
+            false
+        );
+        self::$dom->addChild(
+            $infRPS,
+            'Status',
+            $rps->infStatus,
+            true,
+            'Status',
+            false
+        );
+        self::$dom->addChild(
+            $infRPS,
+            'RegimeEspecialTributacao',
+            $rps->infRegimeEspecialTributacao,
+            true,
+            'RegimeEspecialTributacao',
+            false
+        );
+        
+        $servico = self::$dom->createElement('Servico');
+        $valores = self::$dom->createElement('Valores');
+        self::$dom->addChild(
+            $valores,
+            'ValorServicos',
+            $rps->infValorServicos,
+            true,
+            'ValorServicos',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorPis',
+            $rps->infValorPis,
+            true,
+            'ValorPis',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorCofins',
+            $rps->infValorCofins,
+            true,
+            'ValorCofins',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorInss',
+            $rps->infValorInss,
+            true,
+            'ValorInss',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorIr',
+            $rps->infValorIr,
+            true,
+            'ValorIr',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorCsll',
+            $rps->infValorCsll,
+            true,
+            'ValorCsll',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'IssRetido',
+            $rps->infIssRetido,
+            true,
+            'IssRetido',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorIss',
+            $rps->infValorIss,
+            true,
+            'ValorIss',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'BaseCalculo',
+            $rps->infBaseCalculo,
+            true,
+            'BaseCalculo',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'Aliquota',
+            $rps->infAliquota,
+            true,
+            'Aliquota',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'ValorLiquidoNfse',
+            $rps->infValorLiquidoNfse,
+            true,
+            'ValorLiquidoNfse',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'DescontoIncondicionado',
+            $rps->infDescontoIncondicionado,
+            true,
+            'DescontoIncondicionado',
+            false
+        );
+        self::$dom->addChild(
+            $valores,
+            'DescontoCondicionado',
+            $rps->infDescontoCondicionado,
+            true,
+            'DescontoCondicionado',
+            false
+        );
+        self::$dom->appChild($servico, $valores, 'Adicionando tag Valores em Servico');
+        
+        self::$dom->addChild(
+            $servico,
+            'ItemListaServico',
+            $rps->infItemListaServico,
+            true,
+            'ItemListaServico',
+            false
+        );
+        self::$dom->addChild(
+            $servico,
+            'CodigoCnae',
+            $rps->infCodigoCnae,
+            true,
+            'CodigoCnae',
+            false
+        );
+        self::$dom->addChild(
+            $servico,
+            'CodigoTributacaoMunicipio',
+            $rps->infCodigoTributacaoMunicipio,
+            true,
+            'CodigoTributacaoMunicipio',
+            false
+        );
+        self::$dom->addChild(
+            $servico,
+            'Discriminacao',
+            $rps->infDiscriminacao,
+            true,
+            'Discriminacao',
+            false
+        );
+        self::$dom->addChild(
+            $servico,
+            'MunicipioPrestacaoServico',
+            $rps->infMunicipioPrestacaoServico,
+            true,
+            'MunicipioPrestacaoServico',
+            false
+        );
+        self::$dom->appChild($infRPS, $servico, 'Adicionando tag Servico');
+        
+        $prestador = self::$dom->createElement('Prestador');
+        $cpfCnpj = self::$dom->createElement('CpfCnpj');
+        if ($rps->infPrestador['tipo'] == 2) {
+            self::$dom->addChild(
+                $cpfCnpj,
+                'Cnpj',
+                $rps->infPrestador['cnpjcpf'],
+                true,
+                'Prestador CNPJ',
+                false
+            );
+        } else {
+            self::$dom->addChild(
+                $cpfCnpj,
+                'Cpf',
+                $rps->infPrestador['cnpjcpf'],
+                true,
+                'Prestador CPF',
+                false
+            );
+        }
+        self::$dom->appChild($prestador, $cpfCnpj, 'Adicionando tag CpfCnpj em Prestador');
+        self::$dom->addChild(
+            $prestador,
+            'InscricaoMunicipal',
+            $rps->infPrestador['im'],
+            true,
+            'InscricaoMunicipal',
+            false
+        );
+        self::$dom->appChild($infRPS, $prestador, 'Adicionando tag Prestador em infRPS');
+        
+        $tomador = self::$dom->createElement('Tomador');
+        $identificacaoTomador = self::$dom->createElement('IdentificacaoTomador');
+        $cpfCnpjTomador = self::$dom->createElement('CpfCnpj');
+        if ($rps->infTomador['tipo'] == 2) {
+            self::$dom->addChild(
+                $cpfCnpjTomador,
+                'Cnpj',
+                $rps->infTomador['cnpjcpf'],
+                true,
+                'Tomador CNPJ',
+                false
+            );
+        } else {
+            self::$dom->addChild(
+                $cpfCnpjTomador,
+                'Cpf',
+                $rps->infTomador['cnpjcpf'],
+                true,
+                'Tomador CPF',
+                false
+            );
+        }
+        self::$dom->appChild($identificacaoTomador, $cpfCnpjTomador, 'Adicionando tag CpfCnpj em IdentificacaTomador');
+        self::$dom->addChild(
+            $identificacaoTomador,
+            'RazaoSocial',
+            $rps->infTomador['razao'],
+            true,
+            'RazaoSocial',
+            false
+        );
+        self::$dom->appChild($tomador, $identificacaoTomador, 'Adicionando tag IdentificacaoTomador em Tomador');
+        
+        $endereco = self::$dom->createElement('Endereco');
+        self::$dom->addChild(
+            $endereco,
+            'Endereco',
+            $rps->infTomadorEndereco['end'],
+            true,
+            'Endereco',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Numero',
+            $rps->infTomadorEndereco['numero'],
+            true,
+            'Numero',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Complemento',
+            $rps->infTomadorEndereco['complemento'],
+            true,
+            'Complemento',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Bairro',
+            $rps->infTomadorEndereco['bairro'],
+            true,
+            'Bairro',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Cidade',
+            $rps->infTomadorEndereco['cmun'],
+            true,
+            'Cidade',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Estado',
+            $rps->infTomadorEndereco['uf'],
+            true,
+            'Estado',
+            false
+        );
+        self::$dom->addChild(
+            $endereco,
+            'Cep',
+            $rps->infTomadorEndereco['cep'],
+            true,
+            'Cep',
+            false
+        );
+        self::$dom->appChild($tomador, $endereco, 'Adicionando tag Endereco em Tomador');
+        self::$dom->appChild($infRPS, $tomador, 'Adicionando tag Tomador em infRPS');
+        
+        self::$dom->appChild($root, $infRPS, 'Adicionando tag infRPS em RPS');
+        self::$dom->appendChild($root);
+        $xml = str_replace('<?xml version="1.0" encoding="utf-8"?>', '', self::$dom->saveXML());
+        //header("Content-type: text/xml");
+        //echo $xml;
+        //die;
+        return $xml;
     }
 }
--- src/Models/Issnet/Rps.php
+++ src/Models/Issnet/Rps.php
@@ -22,4 +22,218 @@ use NFePHP\NFSe\Common\Rps as RpsBase;
 
 class Rps extends RpsBase
 {
+    /**
+     * @var array
+     */
+    public $infPrestador = ['tipo' => '', 'cnpjcpf' => '' , 'im' => ''];
+    /**
+     * @var array
+     */
+    public $infTomador = ['tipo' => '', 'cnpjcpf' => '' , 'im' => '', 'razao' => ''];
+    /**
+     * @var array
+     */
+    public $infTomadorEndereco = [
+        'end' => '',
+        'numero' => '',
+        'complemento' => '',
+        'bairro' => '',
+        'cmun' => '',
+        'uf' => '',
+        'cep' => ''
+    ];
+    public $infNumero;
+    public $infSerie;
+    public $infTipo;
+    public $infDataEmissao;
+    public $infNaturezaOperacao;
+    public $infOptanteSimplesNacional;
+    public $infIncentivadorCultural;
+    public $infStatus;
+    public $infRegimeEspecialTributacao;
+    public $infValorServicos;
+    public $infValorPis;
+    public $infValorCofins;
+    public $infValorInss;
+    public $infValorIr;
+    public $infValorCsll;
+    public $infIssRetido;
+    public $infValorIss;
+    public $infBaseCalculo;
+    public $infAliquota;
+    public $infValorLiquidoNfse;
+    public $infDescontoIncondicionado;
+    public $infDescontoCondicionado;
+    public $infItemListaServico;
+    public $infCodigoCnae;
+    public $infCodigoTributacaoMunicipio;
+    public $infDiscriminacao;
+    public $infMunicipioPrestacaoServico;
+    
+    public function prestador($tipo, $cnpjcpf, $im)
+    {
+        $this->prestador = [
+            'tipo' => $tipo,
+            'cnpjcpf' => $cnpjcpf,
+            'im' => $im
+        ];
+    }
+    
+    public function tomador($tipo, $cnpjcpf, $im, $razao)
+    {
+        $this->tomador = [
+            'tipo' => $tipo,
+            'cnpjcpf' => $cnpjcpf,
+            'im' => $im,
+            'razao' => $razao
+        ];
+    }
+    
+    public function tomadorEndereco($end, $numero, $complemento, $bairro, $cmun, $uf, $cep)
+    {
+        $this->tomadorEndereco = [
+            'end' => $end,
+            'numero' => $numero,
+            'complemento' => $complemento,
+            'bairro' => $bairro,
+            'cmun' => $cmun,
+            'uf' => $uf,
+            'cep' => $cep
+        ];
+    }
+    
+    public function numero($value)
+    {
+        $this->infNumero = $value;
+    }
+    
+    public function serie($value)
+    {
+        $this->infSerie = $value;
+    }
+    
+    public function tipo($value)
+    {
+        $this->infTipo = $value;
+    }
+    
+    public function dataEmissao($value)
+    {
+        $this->infDataEmissao = $value;
+    }
+    
+    public function naturezaOperacao($value)
+    {
+        $this->infNaturezaOperacao = $value;
+    }
+    
+    public function optanteSimplesNacional($value)
+    {
+        $this->infOptanteSimplesNacional = $value;
+    }
+    
+    public function incentivadorCultural($value)
+    {
+        $this->infIncentivadorCultural = $value;
+    }
+    
+    public function status($value)
+    {
+        $this->infStatus = $value;
+    }
+    
+    public function regimeEspecialTributacao($value)
+    {
+        $this->infRegimeEspecialTributacao = $value;
+    }
+    
+    public function valorServicos($value)
+    {
+        $this->infValorServicos = $value;
+    }
+    
+    public function valorPis($value)
+    {
+        $this->infValorPis = $value;
+    }
+    
+    public function valorCofins($value)
+    {
+        $this->infValorCofins = $value;
+    }
+    
+    public function valorInss($value)
+    {
+        $this->infValorInss = $value;
+    }
+    
+    public function valorIr($value)
+    {
+        $this->infValorIr = $value;
+    }
+    
+    public function valorCsll($value)
+    {
+        $this->infValorCsll = $value;
+    }
+    
+    public function issRetido($value)
+    {
+        $this->infIssRetido = $value;
+    }
+    
+    public function valorIss($value)
+    {
+        $this->infValorIss = $value;
+    }
+    
+    public function baseCalculo($value)
+    {
+        $this->infBaseCalculo = $value;
+    }
+    
+    public function aliquota($value)
+    {
+        $this->infAliquota = $value;
+    }
+    
+    public function valorLiquidoNfse($value)
+    {
+        $this->infValorLiquidoNfse = $value;
+    }
+    
+    public function descontoIncondicionado($value)
+    {
+        $this->infDescontoIncondicionado = $value;
+    }
+    
+    public function descontoCondicionado($value)
+    {
+        $this->infDescontoCondicionado = $value;
+    }
+    
+    public function itemListaServico($value)
+    {
+        $this->infItemListaServico = $value;
+    }
+    
+    public function codigoCnae($value)
+    {
+        $this->infCodigoCnae = $value;
+    }
+    
+    public function codigoTributacaoMunicipio($value)
+    {
+        $this->infCodigoTributacaoMunicipio = $value;
+    }
+    
+    public function discriminacao($value)
+    {
+        $this->infDiscriminacao = $value;
+    }
+    
+    public function municipioPrestacaoServico($value)
+    {
+        $this->infMunicipioPrestacaoServico = $value;
+    }
 }
