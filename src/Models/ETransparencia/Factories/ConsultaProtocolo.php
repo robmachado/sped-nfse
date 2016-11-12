3cc64fa6428cfae95ad2463069c0802fb002d2eb
Roberto L. Machado <linux.rlm@gmail.com>
2016-11-12 11:24:06 -0200

    [INC] ETransparencia

---
+++ src/Models/ETransparencia/Factories/ConsultaProtocolo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NFePHP\NFSe\Models\ETransparencia\Factories;
+
+use NFePHP\NFSe\Models\ETransparencia\Factories\Header;
+use NFePHP\NFSe\Models\ETransparencia\Factories\Factory;
+
+class ConsultaProtocolo extends Factory
+{
+
+}
---
+++ src/Models/ETransparencia/Factories/ConsultaProtocoloRPS.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace NFePHP\NFSe\Models\ETransparencia\Factories;
+
+use NFePHP\NFSe\Models\ETransparencia\Factories\Header;
+use NFePHP\NFSe\Models\ETransparencia\Factories\Factory;
+
+class ConsultaProtocoloRPS extends Factory
+{
+    
+}
